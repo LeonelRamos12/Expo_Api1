5fe81a20b2caab1d4c80ae7e3fbe8ede54ad9181
Leonel <20240005@ricaldone.edu.sv>
2025-07-10 09:17:48 -0600

    Archivo inicial

---
+++ Expo_Api/src/main/java/Expo_API/Expo_Api/ExpoApiApplication.java
@@ -0,0 +1,13 @@
+package Expo_API.Expo_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpoApiApplication.class, args);
+	}
+
+}
---
+++ Expo_Api/src/test/java/Expo_API/Expo_Api/ExpoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Expo_API.Expo_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
